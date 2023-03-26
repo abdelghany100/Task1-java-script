48b9f3258db1ddd37e2122427b9a09c7d7375afc
abdelghany100 <abdelghaniayman8@gmail.com>
2023-03-27 01:13:50 +0200

    done

---
+++ asisst/index.js
@@ -0,0 +1,361 @@
+          // q-1
+/*var x=prompt('ent your num');
+console.log(x);*/
+
+
+
+          //q-2
+/*var x=prompt('ent your num');
+if(x%3==0 && x%4==0)
+{
+    console.log('Yes');
+}
+else{
+    console.log('No');
+    
+}*/
+
+
+
+
+          //q-3
+/*var x=+prompt();
+
+var y=+prompt();
+if (x >= y) {
+    console.log(x);
+
+    
+}
+else{
+    console.log(y);
+
+}*/
+
+
+
+
+          //q-4
+/*var x=+prompt();
+if (x >= 0) {
+
+    console.log('positive');
+          
+              
+}
+else{
+
+    console.log('negative');
+ }*/
+       
+
+
+
+          //q-5
+/*var x=+prompt();
+var y=+prompt();
+var z=+prompt();
+
+if (x > y && x > z ) {
+    if(z<y)
+    {
+        console.log("max="+x );
+        console.log("min="+z );
+
+    }
+  else{
+    console.log("max="+x );
+    console.log("min="+y );
+  }
+    
+}
+
+if (y > x && y > z ) {
+    if(z<x)
+    {
+        console.log("max="+y );
+        console.log("min="+z );
+
+    }
+  else{
+    console.log("max="+y );
+    console.log("min="+x );
+  }
+    
+}
+if (z > x && z > y ) {
+    if(y<x)
+    {
+        console.log("max="+z );
+        console.log("min="+y );
+
+    }
+  else{
+    console.log("max="+y );
+    console.log("min="+x );
+  }
+    
+}*/
+
+
+
+
+          //q-6
+/*var x=+prompt();
+if(x%2==0)
+{
+    console.log('even');
+}
+else{
+    console.log('odd');
+    
+}*/
+
+
+
+
+          //q-8
+/*var x=prompt();
+switch (x) {
+    case 'o': case 'u': case 'i': case 'e' : case 'a':  
+    console.log('vowel');
+        break;
+
+    default:
+        console.log('constant');
+        break;
+}*/
+
+
+
+
+          //q-9
+/*var x=prompt();
+for ( var i = 1 ; i<=x ;i++) {
+
+ console.log(i);    
+}*/
+
+
+
+
+
+          //q-10
+/*var x=prompt();
+
+
+
+for ( var i = 1 ; i<=12 ;i++) {
+        
+    console.log(x*i); 
+ } */
+
+
+
+
+           //9-11
+/*var x=prompt();
+
+for ( var i = 1 ; i<=x ;i++) {
+
+    if(i%2==0)
+    {
+        console.log(i);
+
+    }    
+}*/
+
+
+
+
+           //9-12
+/*var x=+prompt();
+var y=+prompt();
+var z=+prompt();
+var o=+prompt();
+var n=+prompt();
+var total=(x+y+z+o+n);
+var avrg=(total / 5);
+var perc=(total/500)*100;
+console.log("Total marks = "+total);
+console.log("Average Matks = "+avrg);
+console.log("Percentage = "+perc);*/
+
+
+
+
+
+
+          //13
+/*var x=+prompt('enter month num');
+
+switch (x)
+{
+  case 2:
+    console.log("Days in month: 28");
+    break;
+  case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+    console.log("Days in month: 31");
+    break; case 4:case 6: case 9: case 11:
+    console.log("Days in month: 30");
+    break;
+ 
+}*/
+
+
+
+
+           //14
+/*var x=+prompt();
+if(x>=90)
+{
+    console.log("Percentage = "+x+"%" + '  ' + "Grad A");
+}
+else if(x>=80){
+    console.log("Percentage = "+x+"%" + '  ' + "Grad B");
+   
+}
+else if(x>=70){
+    console.log("Percentage = "+x+"%" + '  ' + "Grad C");
+   
+}
+else if(x>=60){
+    console.log("Percentage = "+x+"%" + '  ' + "Grad D");
+   
+}
+else if(x>=40){
+    console.log("Percentage = "+x+"%" + '  ' + "Grad E");
+   
+}
+else{
+
+    console.log("Percentage = "+x+"%" + '  ' + "Grad F");
+
+}*/
+
+
+
+
+
+           //15
+/*var x=+prompt('enter month num');
+switch (x)
+{
+  case 2:
+    console.log("Days in month: 28");
+    break;
+  case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+    console.log("Days in month: 31");
+    break; case 4:case 6: case 9: case 11:
+    console.log("Days in month: 30");
+    break;
+ 
+}
+*/
+
+
+
+
+           //16
+/*var x=prompt();
+switch (x) {
+    case 'o': case 'u': case 'i': case 'e' : case 'a':  
+    console.log('vowel');
+        break;
+
+    default:
+        console.log('constant');
+        break;
+}*/
+
+
+
+
+           //17
+/*var x=+prompt();
+var y=+prompt();
+
+switch(true) {
+case (x>=y):
+console.log(x);
+break;
+case (y>x):
+    console.log(y);
+        break;   
+
+}*/
+
+
+
+
+          //18
+/*var x=+prompt();
+switch(true){
+
+
+case(x%2==0):
+
+      console.log('even');
+break;
+case(x%2!=0):
+
+      console.log('odd');
+break;
+}*/
+
+
+
+
+
+          //19
+/*var x=+prompt();
+switch(x){
+case  (x > 0) :
+
+    console.log('positive');
+    break;
+          
+    case  (x < 0) :
+
+    console.log('negative');
+    break;
+    default:
+
+    console.log('zero');
+    break;
+               
+
+ }
+*/
+
+
+
+
+
+          //20
+/*var x=+prompt();
+var ch=prompt();
+var y=+prompt();
+switch(true){
+    case (ch=='+'):
+        console.log(x+y);
+        break;
+        case (ch=='-'):
+            console.log(x-y);
+            break;
+            case (ch=='*'):
+                console.log(x*y);
+                break;
+                case (ch=='/'):
+                    console.log(x/y);
+                    break; 
+                    default :
+                    console.log("error");
+                    break;
+
+}*/
+
+
+
+
+
